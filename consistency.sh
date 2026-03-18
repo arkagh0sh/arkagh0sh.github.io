5ef134681972ec65f9b8574fbd4f21ca14f19509
Arka <a.ghosh@uw.edu.pl>
2026-03-18 14:23:02 +0100

    added pages

---
+++ consistency.sh
@@ -0,0 +1,4 @@
+cat header.html index_raw.html > index.html
+cat header.html contact_raw.html > contact.html
+cat header.html publications_raw.html > publications.html
+cat header.html teaching_raw.html > teaching.html
