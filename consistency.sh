575e2768cd034594f24ba6c90ccfcdaf239506ca
Arka <a.ghosh@uw.edu.pl>
2026-03-18 15:46:57 +0100

    added preludium

--- consistency.sh
+++ consistency.sh
@@ -2,3 +2,4 @@ cat header.html index_raw.html > index.html
 cat header.html contact_raw.html > contact.html
 cat header.html publications_raw.html > publications.html
 cat header.html teaching_raw.html > teaching.html
+cat header.html preludium_raw.html > preludium.html
